f5231e43b8b94f68306442f2872906c1327022b3
Andrej Byrtus <a.byrtus99@gmail.com>
2021-09-29 22:44:09 +0200

    fixed powerup import

--- public/js/PowerUp.js
+++ public/js/PowerUp.js
@@ -1,5 +1,5 @@
 import { GameObject } from './GameObject.js';
-import { Effects, Modifier } from './Effects.js';
+import { Modifier } from './Effects.js';
 import { toPixels } from './Util.js';
 
 export class PowerUp extends GameObject {
@@ -7,6 +7,5 @@ export class PowerUp extends GameObject {
     constructor(x, y){
         var newPowerUp = document.createElement("div");
         super(20, 20, newPowerUp, x, y);
-        this.power = 
     }
 }
